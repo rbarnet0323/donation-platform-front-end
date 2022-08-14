0fa300e760caf429c14e6d25b72772af090160ce
rbarnet0323 <rbarnet6@gmail.com>
2022-08-14 08:07:04 -0700

    fixed adsense positioning

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,6 +7,14 @@ export default function Home() {
   return (
     <div class={tw `w-screen h-screen`}>
       <script>document.title="HUMANKIND"</script>
+      <ins className="adsbygoogle"
+        style="display:inline-block;width:728px;height:90px"
+        data-ad-client="ca-pub-1234567890123456"
+        data-ad-slot="1234567890">
+      </ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
       <div class={tw`p-4 mx-auto max-w-screen-md`}>
         <img
           src="/logo.svg"
@@ -18,18 +26,6 @@ export default function Home() {
           file, and refresh.
         </p>
         <Counter start={3} />
-      </div>
-      <div
-        style="position:absolute;bottom:0px;left:0px;right:0px;height:90px">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9175508409686273" crossorigin="anonymous"></script>
-        <ins className="adsbygoogle"
-          style="position:relative;display:inline-block;width:100%;height:90px"
-          data-ad-client="ca-pub-1234567890123456"
-          data-ad-slot="1234567890">
-        </ins>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
       </div>
     </div>
   );
