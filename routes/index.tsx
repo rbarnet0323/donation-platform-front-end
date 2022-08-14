01cf4800ce96358961227220ad3b936019ddf4fb
rbarnet0323 <rbarnet6@gmail.com>
2022-08-14 14:16:31 -0700

    landing page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,10 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw `w-screen h-screen`}>
+    <div class={tw`w-screen h-screen`}>
       <div class={tw`p-4 mx-auto max-w-screen-md`}>
         <ins className="adsbygoogle"
           style="display:inline-block;width:728px;height:90px"
@@ -15,16 +14,22 @@ export default function Home() {
         <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
-        <img
-          src="/logo.svg"
-          width="100px"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class={tw`my-6`}>
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+        <div style="display:flex;border:1px solid black;border-radius:5px">
+          <div style="width:15%;margin-top:auto;margin-bottom:auto;padding:1%">
+            <img
+              src="/logo.svg"
+              height="100%"
+              alt="the fresh logo: a sliced lemon dripping with juice"
+            />
+          </div>
+          <div style="margin-top:auto;margin-bottom:auto">
+            <h1 style="font-weight:bold">HUMANKIND</h1>
+            <p>a platform for compassion...</p>
+          </div>
+        </div>
+        <div style="display:flex;margin-top:2.5%">
+          <p>User not found...</p>
+        </div>
       </div>
     </div>
   );
--- routes/users/[userID].tsx
+++ routes/users/[userID].tsx
@@ -20,7 +20,36 @@ export const handler: Handlers<Object | null> = {
 
 export default function PageComponent({ data }: PageProps<Object | null>) {
   if (!data) {
-    return <h1>User not found...</h1>;
+    return (
+      <div class={tw`w-screen h-screen`}>
+        <div class={tw`p-4 mx-auto max-w-screen-md`}>
+          <ins className="adsbygoogle"
+            style="display:inline-block;width:728px;height:90px"
+            data-ad-client="ca-pub-1234567890123456"
+            data-ad-slot="1234567890">
+          </ins>
+          <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+          <div style="display:flex;border:1px solid black;border-radius:5px">
+            <div style="width:15%;margin-top:auto;margin-bottom:auto;padding:1%">
+              <img
+                src="/logo.svg"
+                height="100%"
+                alt="the fresh logo: a sliced lemon dripping with juice"
+              />
+            </div>
+            <div style="margin-top:auto;margin-bottom:auto">
+              <h1 style="font-weight:bold">HUMANKIND</h1>
+              <p>a platform for compassion...</p>
+            </div>
+          </div>
+          <div style="display:flex;margin-top:2.5%">
+            <p>User not found...</p>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div class={tw`w-screen h-screen`}>
