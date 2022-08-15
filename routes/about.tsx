2c05c9cf1b5ef05aef88ce58c7e649c5ee2c4961
rbarnet0323 <rbarnet6@gmail.com>
2022-08-15 15:07:39 -0700

    terms and conditions

--- routes/about.tsx
+++ routes/about.tsx
@@ -43,6 +43,10 @@ export default function About() {
           <p>2. Once you have created an account with HUMANKIND, you can use the available printer(s) to print your unique QR code.</p>
           <p>3. Whenever somebody uses their phone to scan your QR code, they will be automatically redirected to your profile where they can make a donation to assist you in your journey.</p>
           <p>4. One of our volunteers will mail you your earnings via USPS general delivery.</p>
+          <h1 style="font-weight:bold;margin-top:2.5%">Terms and Conditions:</h1>
+          <p>- We do not condone or accept legal responsibility for any exploitation of HUMANKIND to solicit illegal goods and/or services.</p>
+          <p>- Unfortunately, we cannot accept legal responsibility for your infringement of local panhandling and/or loitering prohibitions.</p>
+          <p>- Any violations of our terms and conditions will result in account suspension.</p>
           <h1 style="font-weight:bold;margin-top:2.5%">Our Team:</h1>
           <p>- Ray Barnett (CTO) <a style="color:blue;text-decoration:underline" href="https://www.linkedin.com/in/ray-barnett-a21b281a6/">LinkedIn</a></p>
           <p>- Phillip Brady (CEO) <a style="color:blue;text-decoration:underline" href="https://www.linkedin.com/in/phillip-brady-a411a1154/">LinkedIn</a></p>
--- routes/signin.tsx
+++ routes/signin.tsx
@@ -36,11 +36,9 @@ export default function SignIn() {
               <a href="https://humankind.ltd">Home</a>
             </div>
           </div>
-          <form method="POST">
-            <input type="email" name="email" placeholder="email address" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%;text-align:center"/>
-            <input type="password" name="password" placeholder="password" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%;text-align:center"/>
-            <button type="submit" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%">Login</button>
-          </form>
+          <input type="email" name="email" placeholder="email address" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%;text-align:center"/>
+          <input type="password" name="password" placeholder="password" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%;text-align:center"/>
+          <button type="submit" style="width:100%;border:1px solid black;border-radius:5px;margin-top:2.5%">Login</button>
         </div>
       </div>
     </Fragment>
